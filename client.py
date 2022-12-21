c920aea065ed213723efde99d23e93d8a3354b6b
Erin Lam <erin.lam168@gmail.com>
2022-12-21 11:48:17 -0500

    Add to main client and server files

---
+++ client.py
@@ -0,0 +1,9 @@
+import socket
+
+##socket is already a library included in python 3
+##we are creating a socket object 
+##socket(socket family type: AF_INIT corresponds to ipv4, type of socket: SOCK_STREAM [streaming socket tcp] )
+s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+s.connect((socket.gethostname(), 1234))
+
+#connects to port 1234
