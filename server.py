fe05a77509901155be1ab32da5a73ea501aec760
Erin Lam <57912076+erinL168@users.noreply.github.com>
2022-12-23 18:10:10 -0500

    moved from main server.py

--- server.py
+++ server.py
@@ -1,49 +1,45 @@
 import socket
+import time
 import pickle
-HEADERSIZE = 10
 
+
+
+HEADERSIZE = 10
 
 ##socket is already a library included in python 3
 ##we are creating a socket object 
 ##socket(socket family type: AF_INIT corresponds to ipv4, type of socket: SOCK_STREAM [streaming socket tcp] )
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-s.connect((socket.gethostname(), 1236))
+# (socket.local host, port number)
 
-#connects to port 1234
+s.bind((socket.gethostname(), 1236))
+s.listen(5) #queue of 5, if messages start stacking up
 
-msg = s.recv(1024) # this is our buffer size
-#we can send anything under 24 bytes
-#why do we need to buffer data, because if we 
-full_msg = "hello"
-while True:
-    full_msg = b"" #bytes msg?
-    new_msg = True
+while True: #we will listen forever for connections
+    clientsocket, address = s.accept() 
+    #anybody connects, then we store the client socket into
+    #clientsocket and their address stored in address (s.accept() is basically another socket)
+    print(f"Connection from {address} has been established!")
+
+
+    d = {1: "hey", 2: "there"}
+    msg = pickle.dumps(d)
+    #print(msg)
+
+    #convert to bytes (utf-8 bytes)
+    msg = bytes(f'{len(msg):<{HEADERSIZE}}', "utf-8") + msg
+
+    #f string that is less than 20 appended with the message
+    #you can append more brackets to more brackets
+    # msg = f'{len(msg):<20}'+msg
+
+    #this basically says the length of message and basically :< means string formatting is to the left, carrot is center ^
+    #msg = f'{len(msg):<{HEADERSIZE}}' + msg
+
+    clientsocket.send(bytes(msg, "utf-8"))
+
     while True:
-        
-        msg = s.recv(16)
-        if new_msg:
-            print(f"new message length:" {msg[:HEADERSIZE]}) # length of message
-            msglen = int(msg[:HEADERSIZE]) #python will allow this (you'd need .strip to get rid of the extra spaces)
-            new_msg = False #not longer than max
-
-        
-        print(f"full message length: {msglen}")
-        #every loop appends to new msg
-        full_msg += msg
-
-        if len(full_msg)-HEADERSIZE == msglen:
-            print("full msg recvd")
-            print(full_msg[HEADERSIZE:])
-
-            d = pickle.loads(full_msg[HEADERSIZE:])
-            print(d)
-
-
-            new_msg = True
-            full_msg = b"" #emptied out we can continue if new messages
-
-        # full_msg += msg.decode("utf-8")
-        
-    print(full_msg)   
-    
-#a byte stream, recieved as bytes, sent as bytes, so all we did is decode it
+        time.sleep(3)
+        msg = f"The time is! {time.time()}"
+        msg = f'{len(msg):<{HEADERSIZE}}'+msg
+        clientsocket.send(bytes(msg, "utsf-8"))
