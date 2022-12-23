6bd44b7436012cb05b96bd7a518363e9ccfb1598
Erin Lam <57912076+erinL168@users.noreply.github.com>
2022-12-23 18:09:35 -0500

    moved from main

--- server.py
+++ server.py
@@ -1,18 +1,49 @@
 import socket
+import pickle
+HEADERSIZE = 10
+
 
 ##socket is already a library included in python 3
 ##we are creating a socket object 
 ##socket(socket family type: AF_INIT corresponds to ipv4, type of socket: SOCK_STREAM [streaming socket tcp] )
 s = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-# (socket.local host, port number)
+s.connect((socket.gethostname(), 1236))
 
-s.bind((socket.gethostname(), 1234))
-s.listen(5) #queue of 5, if messages start stacking up
+#connects to port 1234
 
-while True: #we will listen forever for connections
-    clientsocket, address = s.accept() 
-    #anybody connects, then we store the client socket into
-    #clientsocket and their address stored in address (s.accept() is basically another socket)
-    print(f"Connection from {address} has been established!")
-    clientsocket.send(bytes("Welcome to the server!", "utf-8"))
+msg = s.recv(1024) # this is our buffer size
+#we can send anything under 24 bytes
+#why do we need to buffer data, because if we 
+full_msg = "hello"
+while True:
+    full_msg = b"" #bytes msg?
+    new_msg = True
+    while True:
+        
+        msg = s.recv(16)
+        if new_msg:
+            print(f"new message length:" {msg[:HEADERSIZE]}) # length of message
+            msglen = int(msg[:HEADERSIZE]) #python will allow this (you'd need .strip to get rid of the extra spaces)
+            new_msg = False #not longer than max
+
+        
+        print(f"full message length: {msglen}")
+        #every loop appends to new msg
+        full_msg += msg
+
+        if len(full_msg)-HEADERSIZE == msglen:
+            print("full msg recvd")
+            print(full_msg[HEADERSIZE:])
+
+            d = pickle.loads(full_msg[HEADERSIZE:])
+            print(d)
+
+
+            new_msg = True
+            full_msg = b"" #emptied out we can continue if new messages
+
+        # full_msg += msg.decode("utf-8")
+        
+    print(full_msg)   
     
+#a byte stream, recieved as bytes, sent as bytes, so all we did is decode it
